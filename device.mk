ce7514b53a55e25a602261952cee03c662db50d4
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    ginkgo: bt: Add btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162
    Change-Id: I36ca083bf333826ac8980541181aa52022671396

--- device.mk
+++ device.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0 \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Blur
 TARGET_USES_BLUR := true
