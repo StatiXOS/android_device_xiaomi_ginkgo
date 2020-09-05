17bd85455a58a911293b3fd35b86db86793d5623
Arian <arian.kulmer@web.de>
2020-09-05 14:18:03 +0200

    ginkgo: init: Set hardware revision property
    * The bootloader sets androidboot.hwversion, use this and set
      the ro.boot.hardware.revision property in order to show it
      in settings.
    
    Change-Id: I3c6c678bf055091ea9fcb2a1524d321e2c1be043
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -65,6 +65,9 @@ void vendor_load_properties()
         property_override(string("ro.") + prop + string("build.fingerprint"), fp);
     }
 
+    // Set hardware revision
+    property_override("ro.boot.hardware.revision", hwversion);
+
     // Set hardware SKU prop
     property_override("ro.boot.product.hardware.sku", device);
 
