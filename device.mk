070713039dcb8509dbf8609672659eb0da876d9b
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    ginkgo: Disable the usage of ConfigStore
    Google will deprecate it in S anyway so let's not keep bloat in our manifest.
    Taken from Alex's DT.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ie5febe73c03e95143bbae23f3190e1e870572298

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Config Store
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display/Graphics
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
