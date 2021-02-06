7e983bcced818944428b5db317d792e44cde92bb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-06 22:06:47 +0530

    ginkgo: Remove unused ANT+ support
     * turns out stock doesn't have it, which leads us to assume
       that ginkgo doesn't have this piece of hardware
    
    Change-Id: Icba366c19c67ba24afefbb05267a17385463c063

--- device.mk
+++ device.mk
@@ -95,11 +95,6 @@ PRODUCT_BOARD_PLATFORM := trinket
 # Boot SPL
 BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    antradio_app
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
